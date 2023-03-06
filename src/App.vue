ca5b7001cb01ae1b92bbb45f2668eb217ba089b9
Sergio <sergio.skate89@gmail.com>
2023-03-05 19:13:39 -0600

    Autenticacion

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,60 @@
-<script setup>
+<script>
+import { 
+  getAuth,
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+  } from "firebase/auth";
+
+export default {
+  name: 'Navbar',
+   data() {
+        return {
+            email: '',
+            password: '',
+            repassword: '',
+            errorMessage: ''
+        };
+    },
+   methods: {
+     register(email, password) {
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            alert('¡Registrado!');
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            this.errorMessage = error.message;
+            alert(this.errorMessage);
+            // ..
+        });
+      },        
+     login( email, password ) {
+       const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            alert('¡Sesión iniciada!');
+            // Signed in
+            const user = userCredential.user;
+            // ...
+            })
+        .catch((error) => {
+        const errorCode = error.code;
+        this.errorMessage = error.message;
+        alert(this.errorMessage);
+        });
+        },
+     signout () {
+       const auth = getAuth();
+      signOut(auth).then(() => {
+        alert('¡Sesión cerrada! Inicia sesión.');
+      }).catch((error) => {
+      });
+     }
+   }
+}
 </script>
 
 <template>
@@ -24,8 +80,109 @@
         </li>
       </ul>
     </div>
+    <!-- Después de la etiqueta </form> --> 
+ <!-- Iniciar sesión --> 
+<button type="button" class="btn btn-outline-primary mx-2" 
+  data-bs-toggle="modal" 
+  data-bs-target="#login">
+  Log in
+</button> 
+ <!-- Cerrar sesión -->   
+<button class="btn btn-outline-danger me-2"
+  data-bs-toggle="modal" 
+  data-bs-target="#login"
+  @click="signout">
+  Log out
+</button>
+ <!-- Regístrate --> 
+<button type="button" 
+  class="btn btn-outline-light" 
+  data-bs-toggle="modal" 
+  data-bs-target="#registro"><!-- inicia modal con id="registro" --> 
+  Regístrate
+</button>
   </div>
 </nav>
+<!-- //// Modal - Registrarse //// -->
+<div class="modal fade" id="registro">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Regístrate</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form @submit.prevent="register( this.email, this.password )">  
+           <!-- CORREO -->
+          <div class="input-group mb-3">
+          <span class="input-group-text">Correo</span>
+          <input v-model="email" 
+                  type="email"
+                  required="true"
+                  class="form-control">
+          </div>
+          <!-- PASSWORD -->
+          <div class="input-group mb-3">
+          <span class="input-group-text">Password</span>
+          <input v-model="password" 
+                  type="password"
+                  required="true" 
+                  class="form-control">
+          </div>
+          <!-- REPASSWORD -->
+          <div class="input-group mb-3">
+          <span class="input-group-text">Repite Password</span>
+          <input v-model="repassword" 
+                  type="password"
+                  required="true" 
+                  class="form-control">
+          </div>
+          <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Registrar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- //// Modal - Iniciar sesión //// -->
+<div class="modal fade" id="login">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Inicia de sesión</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form @submit.prevent="login( this.email, this.password )">
+          <!-- CORREO --> 
+          <div class="input-group mb-3">
+          <span class="input-group-text">Correo</span>
+          <input v-model="email" 
+                  type="email"
+                  required="true"
+                  class="form-control">
+          </div>
+          <!-- PASSWORD --> 
+          <div class="input-group mb-3">
+          <span class="input-group-text">Password</span>
+          <input v-model="password" 
+                  type="password"
+                  required="true" 
+                  class="form-control">
+          </div>
+          <div class="d-grid gap-2">
+            <button type="submit" 
+              class="btn btn-primary" 
+              data-bs-dismiss="modal"><!-- Cierra el modal --> 
+              Iniciar sesión
+            </button>
+          </div>
+          </form>
+      </div>
+    </div>
+  </div>
+</div>
 <section class="banner" id="inicio">
     <div class="banner-main">
         <div class="container">
--- src/main.js
+++ src/main.js
@@ -4,3 +4,21 @@ import App from './App.vue'
 import './assets/main.css'
 
 createApp(App).mount('#app')
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUZhXoKyqzA7OMWGIKkx6IKy_2Cp0WpSg",
+  authDomain: "paginasaqq.firebaseapp.com",
+  projectId: "paginasaqq",
+  storageBucket: "paginasaqq.appspot.com",
+  messagingSenderId: "518387442861",
+  appId: "1:518387442861:web:3525bde2d386fc94519224"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
